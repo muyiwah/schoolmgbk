f16f81dd6ea0567643da0e5079d000dafb95e88d
muyiwah <muyiwah457@gmail.com>
2025-10-31 09:22:59 +0100

    latest uodate

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -22,7 +22,7 @@ class AppConstants {
   static String myPin = 'myPin';
   static String hasPinSet = 'hasPinSet';
 
-  // TEST
+  // TEST  asdf 
   // static String dojahWidgetId   = '681e5664b52cf9b2598f5dc9';
   // static String dojahAppId = "6815f136e6f0f1492e449481";
   // static String dojahPublicKey = "test_pk_faF1cixbN2JDQs7eWHQflkPfh";
